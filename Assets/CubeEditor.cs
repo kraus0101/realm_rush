47cba72331e7895cebefdbd909be3120cdcf897d
rlgp91 <nhahaha91@gmail.com>
2018-07-09 12:58:05 +0900

    3. C-sharp lists for custom path

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -10,13 +10,16 @@ public class CubeEditor : MonoBehaviour {
     TextMesh textMesh;
 
 	void Update () {
+        
         Vector3 snapPos;
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
 
         textMesh = GetComponentInChildren<TextMesh>();
-        textMesh.text = snapPos.x/gridSize + "," + snapPos.z/gridSize;
+        string labelBlock = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+        textMesh.text = labelBlock;
+        gameObject.name = labelBlock;
 
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
-	}
+    }
 }
