3f869eed1bd0a308bb8b06593d8e8708a12faecc
rlgp91 <nhahaha91@gmail.com>
2018-07-12 18:12:06 +0900

    14.Tower LookAt Enemy

---
+++ Assets/Tower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+    [SerializeField] Transform objectToPan;
+    [SerializeField] Transform targetEnemy;
+	 
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        objectToPan.LookAt(targetEnemy);
+	}
+}
