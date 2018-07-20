7c15ce515752028f8ac0ac7aae29cb1e7dcfa1da
rlgp91 <nhahaha91@gmail.com>
2018-07-21 01:54:46 +0900

    16.Make Tower shoot (Fix codes that I do myself)

---
+++ Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour {
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int hits = 10;
+    //[SerializeField]Collider collisionMesh;
+
+
+	// Use this for initialization
+	void Start () {
+        AddBoxCollider();
+        OnParticleCollision(gameObject);
+		
+	}
+
+    //void OnCollisionEnter() {
+    //  print("I am hit!");
+    //}
+
+    private void AddBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        //print("I am hit!");
+        hits = hits - 1;
+        if (hits<1) {
+            KillEnemy();
+        }
+
+    }
+
+    private void KillEnemy()
+    {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+        print("Enemy is killed");//todo remove log
+    }
+}
--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,47 +5,13 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour {
 
-    [SerializeField] GameObject deathFX;
-    [SerializeField] Transform parent;
-    [SerializeField]int hits = 10;
-
 	// Use this for initialization
 	void Start () {
-        AddBoxCollider();
-        OnParticleCollision(gameObject);
         PathFinder pathFinder = FindObjectOfType<PathFinder>();
         var path = pathFinder.GetPath();
         StartCoroutine(FollowPath(path));
 
 	}
-
-    private void AddBoxCollider()
-    {
-        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
-        boxCollider.isTrigger = false;
-        print("BoxCollide");//todo remove log
-    }
-
-    private void OnParticleCollision(GameObject other)
-    {
-        ProcessHit();
-        if (hits < 1)
-        {
-            KillEnemy();
-        }
-    }
-
-    private void ProcessHit(){
-        hits = hits - 1;
-    }
-
-    private void KillEnemy()
-    {
-        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
-        fx.transform.parent = parent;
-        Destroy(gameObject);
-        print("enemiy is killed");//todo remove log
-    }
 
     IEnumerator FollowPath(List<Waypoint> path)
     {
