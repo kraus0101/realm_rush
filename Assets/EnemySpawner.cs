f6f83ddf9e198bbf6297d707a17cf174a5907a61
rlgp91 <nhahaha91@gmail.com>
2018-07-22 10:45:58 +0900

    19. Revise Coroutines & Much more

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [SerializeField] float secondsBetweenSpawns = 2f;
+    [SerializeField] EnemyMovement enemyPrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(RepeatedlySpawnEnemies());//start coroutine
+    }
+
+    IEnumerator RepeatedlySpawnEnemies()
+    {
+        while (true)
+        {
+            print("Enemy spawns!");//spawn enemy
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+        }
+    }
+}
+
