c22d00865cd391cbf0561a99bfa644cafd027e6d
rlgp91 <nhahaha91@gmail.com>
2018-08-01 22:12:15 +0900

    31.Unity Post Precessing Stack

---
+++ Assets/Scripts/BaseHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BaseHealth : MonoBehaviour {
+
+    [SerializeField] int health = 10;
+    [SerializeField] int healthDecrease = 1;
+    [SerializeField] Text healthText;
+
+    void Start()
+    {
+        healthText.text = health.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        healthText.text = health.ToString();
+        health -= healthDecrease;
+    }
+}
